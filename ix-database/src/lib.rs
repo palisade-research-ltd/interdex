f3512f8d5513f72c8eef0cd73f8cee66528c0589
IFFranciscoME <if.francisco.me@gmail.com>
2025-07-23 23:56:53 -0600

    Changes for CI

--- ix-database/src/lib.rs
+++ ix-database/src/lib.rs
@@ -8,29 +8,6 @@
 //! - Parquet file storage and partitioning
 //! - System table monitoring and reporting
 //!
-//! ## Example Usage
-//!
-//! ```rust
-//! use ix_database::ClickHouseClient;
-//!
-//! #[tokio::main]
-//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     let client = ClickHouseClient::builder()
-//!         .url("http://localhost:8123")
-//!         .database("default")
-//!         .build()
-//!         .await?;
-//!     
-//!     // Create connection
-//!     let connection = client.create_connection().await?;
-//!     
-//!     // Get system tables
-//!     let tables = client.get_system_tables().await?;
-//!     println!("System tables: {:?}", tables);
-//!     
-//!     Ok(())
-//! }
-//! ```
 
 use std::collections::HashMap;
 use std::sync::Arc;
