dade248312e99431b849d45940436b09096bf443
FranciscoME <if.francisco.me@gmail.com>
2025-08-18 12:14:01 -0600

    13 cex order book features generator (#37)
    * working version
    
    * progress on structure
    
    * Streams and Execution
    
    * multiple data streams working

--- ix-execution/src/bin/collector.rs
+++ ix-execution/src/bin/collector.rs
@@ -9,6 +9,7 @@ use std::{
     time::{Duration, Instant},
 };
 use tokio;
+use tracing::info;
 
 use ix_execution::{
     liquidations::LiquidationNew, queries, trades::TradeNew, ClickHouseClient,
@@ -19,7 +20,7 @@ use ix_cex::{
     models::{exchanges::Exchange, orderbook::TradingPair},
 };
 
-#[tokio::main(flavor = "multi_thread")]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let ch_url = "http://localhost:8123".to_string();
     let ch_db = "default".to_string();
@@ -35,11 +36,11 @@ async fn main() -> anyhow::Result<()> {
         .create_table(&queries::orderbooks::create_tables::create_orderbooks_table_ddl())
         .await;
 
-    // let _ = ch_admin_client
-    //     .create_table(
-    //         &queries::liquidations::create_tables::create_liquidations_table_ddl(),
-    //     )
-    //     .await;
+    let _ = ch_admin_client
+        .create_table(
+            &queries::liquidations::create_tables::create_liquidations_table_ddl(),
+        )
+        .await;
 
     // let _ = ch_admin_client
     //     .create_table(&queries::signals::create_tables::create_signals_table_ddl())
@@ -57,29 +58,37 @@ async fn main() -> anyhow::Result<()> {
         .await
         .unwrap();
 
-    let liquidation_task = tokio::spawn(async move {
-        let symbols = vec!["SOLUSDT".to_string()];
+    let streams_task = tokio::spawn(async move {
+        println!("started streams_task");
 
-        // let streams = vec!["allLiquidation.".to_string(), "publicTrade".to_string()];
-        let streams = vec!["publicTrade".to_string()];
+        let symbols = vec![
+            "SOLUSDT".to_string(),
+            "LINKUSDT".to_string(),
+        ];
+
+        let streams = vec!["allLiquidation".to_string(), "publicTrade".to_string()];
         let source = WssExchange::Bybit;
         let mut rx = stream_data(symbols, streams, source)
             .await
             .expect("failed to open Bybit WS");
 
-        println!("pre while");
-
         while let Some(recv_event) = rx.recv().await {
+            println!("\nrecv_event");
             match recv_event {
                 BybitWssEvent::LiquidationData(event_data) => {
                     let i_liq = LiquidationNew {
                         ts: event_data.liquidation_ts,
-                        symbol: event_data.symbol,
+                        symbol: event_data.symbol.clone(),
                         side: event_data.side,
                         amount: event_data.amount,
                         price: event_data.price,
                         exchange: "bybit".to_string(),
                     };
+
+                    println!(
+                        "\nallLiquidation event received... {:?}",
+                        event_data.symbol
+                    );
 
                     let liquidation_query =
                         queries::liquidations::write_tables::q_insert_liquidations(
@@ -92,7 +101,7 @@ async fn main() -> anyhow::Result<()> {
                 BybitWssEvent::TradeData(event_data) => {
                     let i_trade = TradeNew {
                         trade_ts: event_data.trade_ts,
-                        symbol: event_data.symbol,
+                        symbol: event_data.symbol.clone(),
                         side: event_data.side,
                         amount: event_data.amount,
                         price: event_data.price,
@@ -100,15 +109,16 @@ async fn main() -> anyhow::Result<()> {
                         id: "".to_string(),
                     };
 
+                    println!("\npublicTrade event received... {:?}", event_data.symbol);
+
                     let trade_query =
                         queries::trades::write_tables::q_insert_trades(&i_trade).unwrap();
                     let _ = ch_lq_client.write_table(&trade_query).await;
-                    println!("\ni_trade: \n{:?}", i_trade);
                 }
             }
         }
     });
-    
+
     // --- ORDERBOOKS Data Collector --- //
     let ch_ob_client = ClickHouseClient::builder()
         .url(ch_url.clone())
@@ -122,12 +132,24 @@ async fn main() -> anyhow::Result<()> {
         let mut next_time = Instant::now() + interval;
 
         loop {
-            let v_exchanges =
-                vec![Exchange::Kraken, Exchange::Binance, Exchange::Coinbase];
-            let pair = TradingPair::SolUsdc;
+
+            let v_exchanges = vec![
+                Exchange::Kraken,
+                Exchange::Binance,
+                Exchange::Coinbase
+            ];
+
+            let v_pairs = vec![
+                TradingPair::SolUsdt,
+                TradingPair::LinkUsdt,
+            ];
+
             let depth = 25;
 
             for i_exchange in v_exchanges {
+
+                println!("exchange {:?}", i_exchange);
+
                 let exchange_client: Box<dyn ExchangeClient + Send + Sync> =
                     match i_exchange {
                         Exchange::Binance => Box::new(BinanceClient::new().unwrap()),
@@ -135,25 +157,28 @@ async fn main() -> anyhow::Result<()> {
                         Exchange::Kraken => Box::new(KrakenClient::new().unwrap()),
                     };
 
-                let r_orderbook = exchange_client
-                    .get_orderbook(pair.clone(), Some(depth))
-                    .await
-                    .unwrap();
+                for i_pair in v_pairs.clone() {
+                    println!("pair {:?}", i_pair);
 
-                let query_str: String =
-                    queries::orderbooks::write_tables::q_insert_orderbook(&r_orderbook)
+                    let r_orderbook = exchange_client
+                        .get_orderbook(i_pair.clone(), Some(depth))
+                        .await
                         .unwrap();
-                let _ = ch_ob_client.write_table(&query_str).await;
 
-                println!("\norderbook: {:?}", r_orderbook);
+                    let query_str: String =
+                        queries::orderbooks::write_tables::q_insert_orderbook(&r_orderbook)
+                            .unwrap();
+                    let _ = ch_ob_client.write_table(&query_str).await;
 
-                sleep(next_time - Instant::now());
-                next_time += interval;
+                    sleep(next_time - Instant::now());
+                    next_time += interval;
+
+                }
             }
         }
     });
 
     // Wait for both tasks
-    tokio::try_join!(liquidation_task, orderbook_task)?;
+    tokio::try_join!(streams_task, orderbook_task)?;
     Ok(())
 }
